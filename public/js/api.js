b855c2925529c96c1ad234b4769f4470e1871803
siya-pathak <siya040604@gmail.com>
2022-08-18 18:40:08 +0530

    initial commit

---
+++ public/js/api.js
@@ -0,0 +1,11 @@
+let api_key = "18b9a5cd37c59ab25ac41b581a790243";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+
+let original_img_url = "https://image.tmdb.org/t/p/original";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
+let movie_details_http = "https://api.themoviedb.org/3/movie/{movie_id}?";
